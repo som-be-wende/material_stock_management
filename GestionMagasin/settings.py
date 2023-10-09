3d7bb6d174b950f5cccad7b94470162c091c8610
som-be-wende <107929833+som-be-wende@users.noreply.github.com>
2023-10-09 02:50:39 +0100

    Update settings.py

--- GestionMagasin/settings.py
+++ GestionMagasin/settings.py
@@ -130,8 +130,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'magasinwcs@gmail.com'
-EMAIL_HOST_PASSWORD = 'vcngzmmmwmqgfpqw'
+EMAIL_HOST_USER = '******@gmail.com'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
